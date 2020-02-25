7b24c4d6e45a5de4173172a44a0a6a83d35c1c32
Khaled <khaled@idea.com.tn>
2020-02-25 10:43:25 +0100

    create (personneServie)

---
+++ src/main/java/com/example/FormationGit/service/PersonneService.java
@@ -0,0 +1,10 @@
+package com.example.FormationGit.service;
+
+import com.example.FormationGit.Model.Entity.Person;
+
+public interface PersonneService {
+	
+	
+	public String getLastName(Person p);
+
+}
